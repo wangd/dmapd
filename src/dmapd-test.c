269b8928e736004c18c72f3f9149320f6b66d99c
W. Michael Petullo <mike@flyn.org>
2010-12-11 16:48:00 -0600

    Port unit tests to use check library Signed-off-by: W. Michael Petullo <mike@flyn.org>

--- src/dmapd-test.c
+++ src/dmapd-test.c
@@ -20,6 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <check.h>
 #include <glib.h>
 #include <config.h>
 #include <stdlib.h>
@@ -32,113 +33,30 @@
 #include "dmapd-daap-record-factory.h"
 #include "dmapd-dmap-db-bdb.h"
 
-void test (DMAPDb *db)
+static Suite *dmapd_suite(void)
 {
-	guint id;
-        guint64 filesize;
-        char *location;
-        char *format;
-        char *real_format;
-        char *title;
-        char *album;
-        char *artist;
-        char *genre;
-        gint rating;
-        gint32 duration;
-        gint32 track;
-        gint32 year;
-        gint32 firstseen;
-        gint32 mtime;
-        gint32 disc;
-        gint32 bitrate;
-        gboolean has_video;
+        Suite *s = suite_create("dmapd");
 
-	DMAPRecord *record1 = DMAP_RECORD (
-		g_object_new (TYPE_DMAPD_DAAP_RECORD, 
-			      "location", "aaaaaaaaaaaaaaaaaaaa",
-			      "title", "b",
-			      "songalbum", "c",
-			      "songartist", "d",
-			      "songgenre", "e",
-			      "format", "f",
-			      "real_format", "g",
-			      "rating", 1,
-			      "filesize", 2L,
-			      "duration", 3,
-			      "track", 4,
-			      "year", 5,
-			      "firstseen", 6,
-			      "mtime", 7,
-			      "disc", 8,
-			      "bitrate", 9,
-			      "has-video", TRUE,
-			      NULL));
-	DMAPRecord *record2;
+	TCase *tc_dmapd_daap_record_add_lookup = tcase_create("test_dmapd_daap_record_add_lookup");
 
-	id = dmap_db_add (db, record1);
+	tcase_add_test(tc_dmapd_daap_record_add_lookup, test_dmapd_daap_record_add_lookup);
 
-	record2 = dmap_db_lookup_by_id (db, id);
+	suite_add_tcase(s, tc_dmapd_daap_record_add_lookup);
 
-	g_object_get (record2, "location", &location,
-			       "title", &title,
-			       "songalbum", &album,
-			       "songartist", &artist,
-			       "songgenre", &genre,
-			       "format", &format,
-			       "real_format", &real_format,
-			       "rating", &rating,
-			       "filesize", &filesize,
-			       "duration", &duration,
-			       "track", &track,
-			       "year", &year,
-			       "firstseen", &firstseen,
-			       "mtime", &mtime,
-			       "disc", &disc,
-			       "bitrate", &bitrate,
-			       "has-video", &has_video,
-			       NULL);
-
-	g_assert (! strcmp (location,    "aaaaaaaaaaaaaaaaaaaa"));
-	g_assert (! strcmp (title,       "b"));
-	g_assert (! strcmp (album,       "c"));
-	g_assert (! strcmp (artist,      "d"));
-	g_assert (! strcmp (genre,       "e"));
-	g_assert (! strcmp (format,      "f"));
-	g_assert (! strcmp (real_format, "g"));
-	g_assert (rating    == 1);
-	g_assert (filesize  == 2);
-	g_assert (duration  == 3);
-	g_assert (track     == 4);
-	g_assert (year      == 5);
-	g_assert (firstseen == 6);
-	g_assert (mtime     == 7);
-	g_assert (disc      == 8);
-	g_assert (bitrate   == 9);
-	g_assert (has_video == TRUE);
+	return s;
 }
 
-int main (int argc, char *argv[])
+int main(void)
 {
-	int i = 1;
-	DmapdDAAPRecordFactory *factory;
+	int nf;
 
 	g_type_init ();
 
-	factory = g_object_new (TYPE_DMAPD_DAAP_RECORD_FACTORY, NULL);
+	Suite *s = dmapd_suite();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	nf = srunner_ntests_failed(sr);
+	srunner_free(sr);
 
-	DMAPDb *db = DMAP_DB (object_from_module (TYPE_DMAPD_DMAP_DB,
-						 "ghashtable",
-						 "record-factory",
-						  factory,
-						  NULL));
-
-	for (i = 1; i < 2; i++) {
-		test (db);
-		sleep (1);
-	}
-
-	g_object_unref (db);
-	g_object_unref (factory);
-
-	exit(EXIT_SUCCESS);
+	return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
